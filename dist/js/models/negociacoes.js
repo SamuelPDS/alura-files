7151fdce1c1c954753ce8d12ea343e204126460b
Samuel <duartes607@gmail.com>
2023-12-02 18:36:48 -0300

    teaft: ReadArrayOnly implementation

---
+++ dist/js/models/negociacoes.js
@@ -0,0 +1,12 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    listar() {
+        return this.negociacoes;
+    }
+}
+const negociacoes = new Negociacoes();
