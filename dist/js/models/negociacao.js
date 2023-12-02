1d6669c7812091cd0454d5c90334907dd9c1d787
Samuel <duartes607@gmail.com>
2023-12-02 19:04:54 -0300

    feat: add data clone

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,20 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
+    constructor(_data, quantidade, valor) {
         this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get data() {
-        return this._data;
+        const data = new Date(this._data.getTime());
+        return data;
     }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
+    // get quantidade(): number {
+    //      return this._quantidade
+    //  }
+    // get valor(): number {
+    //      return this._valor
+    //  }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
