fb72f9851a3ca6fe8fe7a39f2433e44a38e060af
Samuel <duartes607@gmail.com>
2023-12-16 13:48:52 -0300

    refac: added strictNullChecks for check if the elements are nulls

--- app/app.ts
+++ app/app.ts
@@ -2,11 +2,16 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 import { NegociacoesView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
-const form = document.querySelector('.form') as Element;
-form.addEventListener('submit', event =>{
-    event.preventDefault(); 
-    controller.adiciona();
-})
+const form = document.querySelector('.form');
+if (form){
+    form.addEventListener('submit', event =>{
+        event.preventDefault(); 
+        controller.adiciona();
+    })    
+} else {
+    throw Error('Não foi possível iniciar a aplicação. Verifiqie se o form existe')
+}
+
 
 
 // const negociacoesView = new NegociacoesView
