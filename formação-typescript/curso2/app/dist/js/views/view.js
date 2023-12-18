c70661d22b02e6d1c2acb887eaa8e3f044b658bf
Samuel <duartes607@gmail.com>
2023-12-18 18:24:28 -0300

    refac: organized the folders for futures files courses

--- formação-typescript/curso2/app/dist/js/views/view.js
+++ formação-typescript/curso2/app/dist/js/views/view.js
@@ -1,28 +1,26 @@
-export abstract class View<T> {
-
-    protected elemento: HTMLElement;
-    private escapar = false;
-
-    constructor(seletor: string, escapar?: boolean) {
+export class View {
+    constructor(seletor, escapar) {
+        this.escapar = false;
         const elemento = document.querySelector(seletor);
         if (elemento) {
-            this.elemento = elemento as HTMLElement;
-        } else {
+            this.elemento = elemento;
+        }
+        else {
             throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
         if (escapar) {
             this.escapar = escapar;
         }
     }
-
-    public update(model: T): void {
+    update(model) {
+        const t1 = performance.now();
         let template = this.template(model);
         if (this.escapar) {
             template = template
                 .replace(/<script>[\s\S]*?<\/script>/, '');
         }
         this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`tempo de execução do método update: ${(t2 - t1) / 1000} segundo`);
     }
-
-    protected abstract template(model: T): string;
 }
