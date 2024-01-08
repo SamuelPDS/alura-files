db75da309f19887def8dc4915e448ff591f729d8
Samuel <duartes607@gmail.com>
2024-01-08 16:58:59 -0300

    feat: add the logic for the keys of the drum kit

--- formação-javascript/aluramidi-curso/main.js
+++ formação-javascript/aluramidi-curso/main.js
@@ -1,5 +1,24 @@
-function playPomSound(){
-    document.querySelector('#som_tecla_pom').play();
+function playKeySound(keyId){
+    document.querySelector(keyId).play();
 }
 
-const KeyList = document.querySelectorAll('.tecla');
+const keyList = document.querySelectorAll('.tecla');
+
+ for (let cont=0; cont < keyList.length; cont++) {
+    const tecla = keyList[cont]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`    //template string
+
+     tecla.onclick = function () {
+        playKeySound(idAudio)
+     };
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    } 
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+ }
