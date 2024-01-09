cb5725ffcb487124e475d80177fadb607e8e613c
Samuel <duartes607@gmail.com>
2024-01-09 13:03:38 -0300

    feat: add conditional for check if the classes were null and spefics the key enter

--- formação-javascript/aluramidi-curso/main.js
+++ formação-javascript/aluramidi-curso/main.js
@@ -1,24 +1,33 @@
-function playKeySound(keyId){
-    document.querySelector(keyId).play();
+function playKeySound(seletorAudio) {
+    const element = document.querySelector(seletorAudio);
+    if (element && element.localName === 'audio') {
+            element.play();
+    } else {
+        alert('elemento ou tag não encontrado, tente novamente');
+    }
 }
 
 const keyList = document.querySelectorAll('.tecla');
 
- for (let cont=0; cont < keyList.length; cont++) {
+for (let cont = 0; cont < keyList.length; cont++) {
     const tecla = keyList[cont]
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`    //template string
 
-     tecla.onclick = function () {
-        playKeySound(idAudio)
-     };
+    tecla.onclick = function () {
+        playKeySound(seletorAudio);
+    }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
-    } 
+    tecla.onkeydown = function (event) {
+        console.log(event.code === 'Space' || event.code === 'Space')
+
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
-    }
 
- }
+    }
+}
