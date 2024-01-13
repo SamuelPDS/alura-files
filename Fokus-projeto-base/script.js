312bdc95a9721ea1badadf6a0081e937bf098cc5
Samuel <duartes607@gmail.com>
2024-01-13 14:51:44 -0300

    feat: add code to change the image, background and button style based on click of the component

---
+++ Fokus-projeto-base/script.js
@@ -0,0 +1,58 @@
+const html = document.querySelector('html');
+const btnFoco = document.querySelector('.app__card-button--foco');
+const btnCurto = document.querySelector('.app__card-button--curto');
+const btnLongo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const btns = document.querySelectorAll('.app__card-button');
+
+
+btnFoco.addEventListener('click', () => {
+    alterarContexto('foco')
+    btnFoco.classList.add('active');
+})
+
+btnCurto.addEventListener('click', () => {
+    alterarContexto('descanso-curto');
+    btnCurto.classList.add('active');
+})
+
+btnLongo.addEventListener('click', () => {
+    alterarContexto('descanso-longo');
+    btnLongo.classList.add('active');
+})
+
+function alterarContexto(contexto) {
+    btns.forEach(function(contexto){
+        contexto.classList.remove('active');
+    })
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `imagens/${contexto}.png`);
+    
+
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            
+            `
+            break;
+
+        case "descanso-curto":
+            titulo.innerHTML = ` Que tal dar uma respirada? <br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+
+        case "descanso-longo":
+            titulo.innerHTML = ` Hora de voltar à superfície. <br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
+}
+
