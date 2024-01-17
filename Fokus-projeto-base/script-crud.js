a4d0b80e98d0b02c00d587ea028ca919b2b41d75
Samuel <duartes607@gmail.com>
2024-01-17 20:12:10 -0300

    feat: add the html by js

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -3,7 +3,39 @@
 const btnAddTask = document.querySelector('.app__button--add-task');
 const formAddTask = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
-const tasks = []
+const ulTask = document.querySelector('.app__section-task-list');
+const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+
+function createTaskElement(task){
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = `
+    <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg> 
+    `
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = task.description;
+
+    const button = document.createElement('button');
+    const imgButton = document.createElement('img');
+    imgButton.setAttribute('src', 'imagens/edit.png');
+
+    button.append(imgButton);
+
+    li.append(svg)
+    li.append(paragraph)
+    li.append(button)
+
+    return li
+}
 
 btnAddTask.addEventListener('click', () => {
     // if(formAddTask) {
@@ -17,5 +49,11 @@ formAddTask.addEventListener('submit', (e) => {
         description: textArea.value
     }
     tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 })
+
+tasks.forEach(task => {
+    const taskElement = createTaskElement(task);
+    ulTask.append(taskElement)
+});
+
