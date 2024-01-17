9664c87fd0cda184e8a86d8977089e677242a75e
Samuel <duartes607@gmail.com>
2024-01-16 21:24:56 -0300

    feat: add function at the submit of the addEventListener

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -1,10 +1,21 @@
 //encontrar btn adicionar tarefa
 
 const btnAddTask = document.querySelector('.app__button--add-task');
-const formAddTask = document.querySelector('.app__form-add-task')
+const formAddTask = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+const tasks = []
 
 btnAddTask.addEventListener('click', () => {
     // if(formAddTask) {
         formAddTask.classList.toggle('hidden');
     // }
+})
+
+formAddTask.addEventListener('submit', (e) => {
+    e.preventDefault(); //prevenir a tela de atualizar
+    const task = {
+        description: textArea.value
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', tasks)
 })
