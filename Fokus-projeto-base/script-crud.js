f0582afcfcfc255846d0a9932bfb0af2ebb98582
Samuel <duartes607@gmail.com>
2024-01-18 21:44:38 -0300

    refac: add class to form and a hidden when task is added

--- Fokus-projeto-base/script-crud.js
+++ Fokus-projeto-base/script-crud.js
@@ -13,20 +13,21 @@ function createTaskElement(task){
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg> 
+        
     `
 
     const paragraph = document.createElement('p');
     paragraph.textContent = task.description;
+    paragraph.classList.add('class', 'app__section-task-list-item-description');
 
     const button = document.createElement('button');
     const imgButton = document.createElement('img');
     imgButton.setAttribute('src', 'imagens/edit.png');
+    imgButton.classList.add('class', 'app_button-edit')
 
     button.append(imgButton);
 
@@ -49,7 +50,12 @@ formAddTask.addEventListener('submit', (e) => {
         description: textArea.value
     }
     tasks.push(task);
+    const TaskElement = createTaskElement(task);
+    ulTask.append(TaskElement);
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    textArea.value = '';
+    formAddTask.classList.add('hidden');
+
 })
 
 tasks.forEach(task => {
