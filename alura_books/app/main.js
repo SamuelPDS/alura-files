003af294b16a999798979586f9be27523114c42e
SamuelPDS <duartes607@gmail.com>
2024-02-06 17:35:17 -0300

    feat: add the code to get the discount

--- alura_books/app/main.js
+++ alura_books/app/main.js
@@ -7,24 +7,6 @@ getBooksFromAPI()
 async function getBooksFromAPI() {
     const res = await fetch(endpointApi)
     books = await res.json()
-    console.table(books)
-    showBooksInScreen(books)
-}
-
-function showBooksInScreen(bookList) {
-    bookList.forEach(book => {
-        elementToInsertElements.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src=${book.imagem} alt=${book.alt} />
-        <h2 class="livro__titulo">
-            ${book.titulo}
-        </h2>
-        <p class="livro__descricao">${book.autor}</p>
-        <p class="livro__preco" id="preco">R$ ${book.preco}</p>
-        <div class="tags">
-          <span class="tag">${book.categoria}</span>
-        </div>
-      </div>
-        `
-    });
+    let booksWithDiscounts = applyDiscount(books)
+    showBooksInScreen(booksWithDiscounts)
 }
