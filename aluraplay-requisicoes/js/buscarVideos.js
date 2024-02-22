3b5c3a70ed81752f65f47c893002dda2c43b8800
Samuel <duartes607@gmail.com>
2024-02-22 13:07:46 -0300

    feat: add other way to do the video search

--- aluraplay-requisicoes/js/buscarVideos.js
+++ aluraplay-requisicoes/js/buscarVideos.js
@@ -1,5 +1,12 @@
-import { conectarApi } from "./conectar";
+import { conectarApi } from "./conectar.js";
 
-async function buscarVideo() {
-    const dadosDePesquisa = document.querySelector('[data-pesquisa]')
+
+async function buscarVideo(e) {
+    e.preventDefault();
+    const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
+    const busca = await conectarApi.buscaVideo(dadosDePesquisa)
 }
+
+const botaoPesquisa = document.querySelector('[data-pesquisa]')
+
+botaoPesquisa.addEventListener('click', e => buscarVideo(e))
