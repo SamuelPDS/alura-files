72e1b7e12fdc9650b1fc2759e9bb1a13f67542c7
Samuel <duartes607@gmail.com>
2024-02-28 18:48:18 -0300

    feat: add the function to verify the cpf

--- monibank-main/js/valida-cpf.js
+++ monibank-main/js/valida-cpf.js
@@ -1,6 +1,12 @@
 export default function ehUmCpf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    ValidaRepetidos(cpf)
+    if(ValidaPrimeiroDigito(cpf) || ValidaRepetidos(cpf) ||  ValidaSegundoDigito(cpf)) {
+        console.log('Esse cpf não existe')
+    } else {
+        console.log('Esse cpf existe')
+    }
+
+
     console.log(ValidaRepetidos(cpf))
 }
 
@@ -19,4 +25,42 @@ function ValidaRepetidos(cpf) {
  ]
 
     return numRepetidos.includes(cpf)
+}
+
+
+function ValidaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma==11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9]
+}
+
+
+function ValidaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11
+
+    if (soma == 10 || soma==11) {
+        soma = 0;
+    }
+
+    console.log( soma != cpf[10])
 }
