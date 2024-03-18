db07c70c2ad4c3de042757927f8215cbfc623556
Samuel <duartes607@gmail.com>
2024-03-18 08:06:12 -0300

    feat: finished the logical to add the value in the historic and clean, according the button click

--- praticando-logica/carrinho-compras/js/app.js
+++ praticando-logica/carrinho-compras/js/app.js
@@ -1,17 +1,52 @@
 const formTag = document.querySelector('.formulario')
-const btnAdd = document.querySelector('.botao-adicionar')
 const productElementt = document.querySelector('#produto')
-const quantelement = document.querySelector('#quantidade')
+const titleHistoric = document.querySelector('.product-historic')
+const totalPrice = document.querySelector('.carrinho__total')
+let value = 1400
 
 
 formTag.addEventListener('submit', e => {
     e.preventDefault();
-        const productValue = e.target.elements["produto"].value
+        let productInfo = e.target.elements["produto"].value
         const quantValue = e.target.elements["quantidade"].value
 
-    post(productValue, quantValue)
+        productInfo = productInfo.replace('-', '').replace('R$', '')
+        productInfo = productInfo.split("  ")
+        console.log(productInfo[1])
+
+        const productName = productInfo[0]
+        const productPrice = parseInt(productInfo[1])
+
+    if(quantValue === '') {
+        alert('Digite uma quantidade!')
+    } else {
+        post(productName, productPrice, quantValue)
+    }
+
 } )
 
-function post(product, quant) {
-    
+formTag.addEventListener('reset', e => {
+    e.preventDefault()
+   titleHistoric.innerHTML = '' 
+   totalPrice.innerHTML = `
+    Total: <span class="texto-azul" id="valor-total"></span> 
+    `
+   value = 0
+})
+
+function post(product, price, quant) {
+
+   titleHistoric.innerHTML += `
+   <section class="carrinho__produtos" id="lista-produtos">
+   <section class="carrinho__produtos__produto">
+     <span class="texto-azul">${quant}x</span> ${product} <span class="texto-azul">R$${price}</span>
+   </section>
+ </section>
+   ` 
+
+   value += (price * quant) 
+   totalPrice.innerHTML = `
+    Total: <span class="texto-azul" id="valor-total">R$${value}</span> 
+    `
+
 }
