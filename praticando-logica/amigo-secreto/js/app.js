1658c1673765974b03bf20dac173981069d09701
Samuel <duartes607@gmail.com>
2024-03-24 18:29:27 -0300

    feat: add the funtion to reset and draw btn(draw is repeating values)

--- praticando-logica/amigo-secreto/js/app.js
+++ praticando-logica/amigo-secreto/js/app.js
@@ -1,7 +1,9 @@
-const formElement = document.querySelector('.form')
-const btnSendData = formElement.getElementsByClassName('button')[0]
-const friendsList = document.querySelector('#lista-amigos')
+const formElement = document.querySelector('.form');
+const btnSendData = formElement.getElementsByClassName('button')[0];
+const friendsList = document.querySelector('#lista-amigos');
 let formInput = document.querySelector('.form__input');
+let nameDrawnList = document.querySelector('.prizeDraw__container');
+const resetBtn = document.querySelector('.form__link')
 let names = []
 
 // console.log(formElement)
@@ -38,7 +40,26 @@ function Post(data) {
 
 
 function Draw(names){
-    
-    let randomName = names[Math.floor(Math.random() * names.length)]
-    console.log(randomName)
+    for(i = 0; i< names.length; i++){
+        let nameDrawn = names[i]
+        let actualValue = []
+        let randomName = names[Math.floor(Math.random() * names.length)]
+        if(!actualValue.includes(randomName)) {
+            actualValue.push(randomName)
+        } else {
+            return randomName
+        }
+        nameDrawnList.innerHTML += `
+        <p id="lista-sorteio">${nameDrawn} -> ${actualValue}</p>  
+        `
+        console.log('foi')
+    }
+}
+
+
+resetBtn.addEventListener('click', ResetValues)
+
+function ResetValues() {
+    friendsList.textContent = ''
+    nameDrawnList.innerHTML = ''
 }
