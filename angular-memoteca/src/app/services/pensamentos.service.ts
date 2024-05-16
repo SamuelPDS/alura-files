ec4774f0cb5e7bf845adfb4e49a8606042f3d8d9
Samuel <duartes607@gmail.com>
2024-05-15 22:11:37 -0300

    pudate:adicionar requisicao para deletar e retornar pensamento pelo id

--- angular-memoteca/src/app/services/pensamentos.service.ts
+++ angular-memoteca/src/app/services/pensamentos.service.ts
@@ -13,11 +13,19 @@ export class PensamentosService {
   private readonly API_URL = "http://localhost:3000/pensamento"
 
   getPensamentos(): Observable<Pensamento[]> {
-    return this.httpClient.get<Pensamento[]>(this.API_URL)
+    return this.httpClient.get<Pensamento[]>(this.API_URL);
   }
 
   createPensamentos(pensamento: Pensamento): Observable<Pensamento> {
-    return this.httpClient.post<Pensamento>(this.API_URL, pensamento)
+    return this.httpClient.post<Pensamento>(this.API_URL, pensamento);
+  }
+
+  deleteService(id: number): Observable<Pensamento> {
+    return this.httpClient.delete<Pensamento>(`${this.API_URL}/${id}`);
+  }
+
+  getPensamentoById(id: string) {
+    return this.httpClient.get<Pensamento>(`${this.API_URL}/${id}`)
   }
 
 }
