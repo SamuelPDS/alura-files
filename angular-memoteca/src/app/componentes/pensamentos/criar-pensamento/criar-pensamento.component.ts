159f6fb116b110b7365af77ea6942fe7d8346206
Samuel <duartes607@gmail.com>
2024-05-19 12:47:45 -0300

    feat: iniciando com formularios reativos

--- angular-memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular-memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Pensamento } from './../pensamento';
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { Route, Router } from '@angular/router';
 import { PensamentosService } from 'src/app/services/pensamentos.service';
 
@@ -11,20 +12,30 @@ import { PensamentosService } from 'src/app/services/pensamentos.service';
 })
 
 export class CriarPensamentoComponent implements OnInit {
+formGroup!: FormGroup;
 
-  card: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  // card: Pensamento = {
+  //   conteudo: '',
+  //   autoria: '',
+  //   modelo: ''
+  // }
 
-  constructor(private route: Router, private pensamentoService: PensamentosService) { }
+  constructor(private route: Router, private pensamentoService: PensamentosService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.createForm(pensamento: Pensamento)
+  }
+
+  createForm(pensamento: Pensamento) {
+    this.formGroup = this.formBuilder.group({
+      conteudo: [pensamento.conteudo, [Validators.required]],
+      autoria: [pensamento.autoria, Validators.required],
+      modelo: [pensamento.modelo, Validators.required]
+    })
   }
 
   createThinking(){
-    this.pensamentoService.createPensamentos(this.card).subscribe(res => {
+    this.pensamentoService.createPensamentos(this.formGroup.value).subscribe(res => {
       alert("Pensamento adicionado")
       this.route.navigate([''])
     })
