bd44510471cfba0f2ebdcc8f2f6fd55741ee6faf
Samuel <duartes607@gmail.com>
2024-05-29 21:08:18 -0300

    usando arquivos disponibilizados pela plataforma pois os desenvolvidos deram bugs

--- ng-test/src/app/shared/services/unique-id/unique-id.service.ts
+++ ng-test/src/app/shared/services/unique-id/unique-id.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UniqueIdService {
-  private numberOfGeneratedIds = 0;
-  static generateUniqueIdWithPrefix: any;
 
-  private validID = /^[A-Za-z]+[\w\-\:\.]*$/;
+  private numberOfGeneratedIds = 0;
+
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
   public generateUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix || !this.validID.test(prefix)) {
-      throw Error('Prefix can not be empty!')
+    if (!prefix || !this.validId.test(prefix)) {
+      throw Error('Prefix can not be empty');
     }
     const uniqueId = this.generateUniqueId();
     this.numberOfGeneratedIds++;
