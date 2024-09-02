472788c1d75e86659e8f10b35a0312fa6d9926c2
Samuel <duartes607@gmail.com>
2024-09-02 07:39:15 -0300

    ends the course

--- ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -8,6 +8,7 @@ import { LikeWidgetModule } from './like-widget.module';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null; //wrapper que embrulha o component para testes
+  let component: LikeWidgetComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,24 +19,30 @@ describe(LikeWidgetComponent.name, () => {
     }).compileComponents(); //aguarda a compilação do component ser renderizado
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
   it('shoud create component', () => {
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
-  it('should generate a Id when the same was not defined at method param', () => {
-    const component = fixture.componentInstance;
+  it('should auto generate a Id when the onInit int called when input id was not defined at method param', () => {
     fixture.detectChanges()
     expect(component.id).toBeTruthy();
   })
 
   it('should not generate a Id when the same is defined at method param', () => {
-    const component = fixture.componentInstance;
     component.id = 1;
     fixture.detectChanges(); //é recomendado inserir em todos os testes
     expect(component.id).toBe(1);
+  })
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger emission when called`, () => {
+      spyOn(component.liked, 'emit');
+      fixture.detectChanges();
+      component.like();
+      expect(component.liked.emit).toHaveBeenCalled();
   })
 });
 
